820d25d3f837199a4b51c473951210da52b1b47e
Dmitriy Yaryshev <krestovcode@gmail.com>
2023-12-18 10:14:49 +0300

    правки

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 
 menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('.menu--active')
+    menu.classList.toggle('menu--active')
 });
