26cc2f4f4833f54dcd0b457e2edc4e0672a3658d
Demitriy <140967862+Vantengu1@users.noreply.github.com>
2023-12-22 14:03:09 +0300

    добавил всплывающее окно

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const menu = document.querySelector('.menu');
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('menu--active');
 });
+
+let data = prompt("Сколько тебе лет?");
+if(data >= 18) {
+     alert("Отлично выглядишь!");
+ } else {
+     alert("Мне кажется ещё рано.");
+ }
+ console.log(data);
